721ba445c6408170e0f63789aba419576fb8804d
zhangliangwang <zhangliangwang@dd.git>
2017-06-12 09:17:59 +0800

    Initial Commit

---
+++ YYTableViewLearn/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  YYTableViewLearn
+//
+//  Created by Ryan on 2017/6/12.
+//  Copyright © 2017年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ YYTableViewLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYTableViewLearn
+//
+//  Created by Ryan on 2017/6/12.
+//  Copyright © 2017年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
